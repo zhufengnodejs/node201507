2ee204b6db32c126b0c6007813ea10627361881d
zhangrenyang-t510 <zhang_renyang@>
2015-10-18 12:51:21 +0800

    proto

---
+++ 6.jsadvance/9.callapply.js
@@ -0,0 +1,24 @@
+/**
+  call  以不同的对象作为上下文来调用某个函数，允许 一个对象调用另外一个对象的成员函数
+   apply
+ bind
+ **/
+var person = {
+    name:'zfpx',
+    say:function(words,world){
+        console.log(this.name+' say '+words,world);
+    }
+}
+
+person.say('hello');// zfpx say hello
+
+var p = {
+    name:'node.js'
+}
+
+person.say.call(p,'hello');
+person.say.apply(p,['hello']);
+
+var newSay = person.say.bind(p,'myhello');
+newSay('world');
+
