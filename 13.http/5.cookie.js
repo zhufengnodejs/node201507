b5ec2aac9d4d77e8db2f165dfed17629d8eb63ea
zhangrenyang-t510 <zhang_renyang@>
2015-10-25 12:30:42 +0800

    设置和读取cookie

---
+++ 13.http/5.cookie.js
@@ -0,0 +1,46 @@
+
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+/**
+ * Domain 域名
+ * Path 路径 控制访问哪些路径的时候发送cookie
+ *
+ * Expires/Max-Age 设置cookie过期时间或有效期的
+ * HTTP 为了安全，不允许javascript修改cookie
+ * Secure 只限于https服务器使用
+ *
+ * Cookie:name=zfpx; name2=zfpx2
+ */
+http.createServer(function(req,res) {
+    var urlObj = url.parse(req.url);
+    var pathname = urlObj.pathname;
+    if (pathname == '/favicon.ico') {
+        res.end('404');
+    } else if (pathname == '/write') {
+        //res.setHeader('Set-Cookie','name=zfpx; Expires='+new Date(new Date().getTime()+20000).toGMTString());//设置一个header ,名称固定就叫Set-Cookie
+        //res.setHeader('Set-Cookie','name=zfpx; Max-Age='+20);//设置一个header ,名称固定就叫Set-Cookie
+        res.setHeader('Set-Cookie','name=zfpx; HttpOnly');//只能通过HTTP查看，不能通过js操作
+        res.end('write');
+    }else if(pathname == '/read'){
+        var cookie = req.headers.cookie;
+        //name=zfpx&name2=zfpx2 name=zfpx; name2=zfpx2
+        var cookieObj = querystring.parse(cookie,'; ');
+        console.log(cookieObj);
+        if(cookieObj && cookieObj.name){
+            res.end('old friend');
+        }else{
+            res.end('new friend');
+        }
+    }else if(pathname == '/read2'){
+        var cookie = req.headers.cookie;
+        //name=zfpx&name2=zfpx2 name=zfpx; name2=zfpx2
+        var cookieObj = querystring.parse(cookie,'; ');
+        console.log(cookieObj);
+        if(cookieObj && cookieObj.name){
+            res.end('old friend');
+        }else{
+            res.end('new friend');
+        }
+    }
+}).listen(8080);
