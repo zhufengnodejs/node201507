dea0b22df9d49e4329d185d88d995acab2ece897
zhangrenyang-t510 <zhang_renyang@>
2015-10-25 11:17:16 +0800

    支持上传图片

--- 13.http/4.server.js
+++ 13.http/4.server.js
@@ -2,6 +2,8 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var querystring = require('querystring');
+var formidable = require('formidable');
+var mime = require('mime');
 /**
  * req
  * res
@@ -17,16 +19,17 @@ http.createServer(function(req,res){
     }else if(pathname == '/'){
       fs.createReadStream('./reg.html').pipe(res);
     }else if(pathname == '/reg'){
-        var result = '';
-        req.on('data',function(data){
-            result+=data;
-        });
-        // result = username=zfpx&password=123
-        req.on('end',function(){
-            //把查询字符串转成用户对象 {username:zfpx,password:123}
-            var userObj = querystring.parse(result);
+        var parser = new formidable.IncomingForm();
+        parser.parse(req,function(err,fields,files){
+            console.log(fields);//普通 input
+            console.log(files);//文件类型的字段
+            var userObj = {username:fields.username,password:fields.password};
             userList.push(userObj);
-            fs.createReadStream('./login.html').pipe(res);
+            fs.createReadStream(files.headlike.path).pipe(fs.createWriteStream('./upload/'+files.headlike.name));
+            res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+            res.write(userObj.username+'<br/>');
+            res.write(userObj.password+'<br/>');
+            res.end('<img src="/upload/'+files.headlike.name+'"/>');
         });
     }else if(pathname == '/login'){
         var result = '';
@@ -49,5 +52,8 @@ http.createServer(function(req,res){
             //如果没有找到合法用户，则显示注册表单
             fs.createReadStream('./reg.html').pipe(res);
         });
+    }else if(pathname.indexOf('/upload/')!=-1){
+        res.writeHead(200,{'Content-Type':mime.lookup(pathname)});
+        fs.createReadStream('.'+pathname).pipe(res);
     }
 }).listen(8080);
